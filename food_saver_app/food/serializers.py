790924d3b392eaef421e2f6624864502f9640408
Maria.Kowalska <maria.kowalska@itechart-group.com>
2023-03-13 19:51:29 +0100

    add ProductAdmin, improve serializer

--- food_saver_app/food/serializers.py
+++ food_saver_app/food/serializers.py
@@ -4,6 +4,11 @@ from .models import Products
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    category = serializers.SerializerMethodField()
+
     class Meta:
         model = Products
         fields = ["name", "category"]
+
+    def get_category(self, obj: Products) -> str:
+        return obj.get_category_display()
